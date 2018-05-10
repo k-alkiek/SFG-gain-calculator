a9e86be86bd855cb88255b908c2281bb0f529f28
Hesham Medhat <heshammedhat5@gmail.com>
2018-05-10 22:14:23 +0200

    Set up data containers for display

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,3 +49,24 @@ function resetButtons() {
 	document.getElementById("input").className = "btn btn-primary btn-lg"
 	document.getElementById("output").className = "btn btn-primary btn-lg"
 }
+
+function openResultElement(evt, elementID) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(elementID).style.display = "block";
+    evt.currentTarget.className += " active";
+}
